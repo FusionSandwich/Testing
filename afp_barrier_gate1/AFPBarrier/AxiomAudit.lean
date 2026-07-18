d1a4cbc398b13831b2a3e0ad4f21a584b959a02e
FusionSandwich <145234893+FusionSandwich@users.noreply.github.com>
2026-07-18 01:16:55 -0400

    Expand axiom audit for Gate 2

--- afp_barrier_gate1/AFPBarrier/AxiomAudit.lean
+++ afp_barrier_gate1/AFPBarrier/AxiomAudit.lean
@@ -11,6 +11,10 @@ import AFPBarrier
 #print axioms AFPBarrier.jumpGenerator_sq_le_rate_mul_carre
 #print axioms AFPBarrier.zero_carreDuChamp_forces_generator_zero
 
+#print axioms AFPBarrier.jumpGenerator_chain_rule_defect
+#print axioms AFPBarrier.strict_tangent_chain_rule_forces_generator_zero
+#print axioms AFPBarrier.square_chain_rule_forces_generator_zero
+
 #print axioms AFPBarrier.no_exact_linear_and_square_at_peak
 #print axioms AFPBarrier.no_exact_shifted_quadratic_at_peak
 
@@ -19,6 +23,11 @@ import AFPBarrier
 #print axioms AFPBarrier.eigenvalue_sq_le_rate_mul_peakDefect
 #print axioms AFPBarrier.peakDefect_pos
 #print axioms AFPBarrier.peakDefect_lower_bound
+
+#print axioms AFPBarrier.weightedAdjointRate_nonneg
+#print axioms AFPBarrier.jumpGenerator_weightedAdjointRate_eq
+#print axioms AFPBarrier.jumpGenerator_weightedAdjointRate_eigen
+#print axioms AFPBarrier.no_exact_forward_linear_and_square_at_peak
 
 #print axioms AFPBarrier.no_exact_S2_degree_two_at_peak
 #print axioms AFPBarrier.S2_four_le_rate_mul_defect
