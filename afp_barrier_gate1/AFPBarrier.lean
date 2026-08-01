9feb8754723b9d88b145e3c09b807cd095a79e35
FusionSandwich <145234893+FusionSandwich@users.noreply.github.com>
2026-08-01 14:05:09 -0400

    Import spherical feasibility and duality modules

--- afp_barrier_gate1/AFPBarrier.lean
+++ afp_barrier_gate1/AFPBarrier.lean
@@ -7,6 +7,9 @@ import AFPBarrier.SphericalNetScaling
 import AFPBarrier.LossVariance
 import AFPBarrier.LossVarianceSharpness
 import AFPBarrier.LocalSphericalFeasibility
+import AFPBarrier.AntipodalSphericalFeasibility
+import AFPBarrier.LocalSphericalBounds
+import AFPBarrier.SharedEdgeGeometry
 import AFPBarrier.GlobalLossRigidity
 import AFPBarrier.ExplicitEulerTransport
 import AFPBarrier.SpatialUpwindTransport
@@ -17,6 +20,7 @@ import AFPBarrier.ForwardAdjoint
 import AFPBarrier.ReversibleConductance
 import AFPBarrier.CompleteGraph
 import AFPBarrier.DualCertificate
+import AFPBarrier.DualComplementarity
 import AFPBarrier.ImplementationConvention
 import AFPBarrier.ScalingCompatibility
 import AFPBarrier.NormalizationAudit
