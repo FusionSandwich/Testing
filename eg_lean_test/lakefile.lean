3feda2d5d68c8e32e094b1b0fb8514487d5db36f
FusionSandwich <145234893+FusionSandwich@users.noreply.github.com>
2026-08-16 14:19:05 -0400

    Add the matching mathlib v4.33.0 dependency

--- eg_lean_test/lakefile.lean
+++ eg_lean_test/lakefile.lean
@@ -3,6 +3,9 @@ open Lake DSL
 
 package erdos_gyarfas
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.0"
+
 @[default_target]
 lean_lib ErdosGyarfas where
   srcDir := "formal"
