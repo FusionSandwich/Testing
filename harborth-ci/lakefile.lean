a22f33bb00652ec61c495f9c3caf5f751d721a38
FusionSandwich <145234893+FusionSandwich@users.noreply.github.com>
2026-08-23 09:21:10 -0400

    harborth-ci: add pinned Jordan-Schoenflies topology dependency

--- harborth-ci/lakefile.lean
+++ harborth-ci/lakefile.lean
@@ -5,7 +5,11 @@ package «harborth» where
   version := v!"0.1.0"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.0"
+  "https://github.com/leanprover-community/mathlib4.git" @ "905b95818eb32af7874a58b427f50c1711a5e96c"
+
+require classificationOfSurfaces from git
+  "https://github.com/mccorvie/classification-of-surfaces.git" @
+    "bb5db8b116d9c8fc2845123e8f724d0ca38a5378"
 
 @[default_target]
 lean_lib Harborth where
